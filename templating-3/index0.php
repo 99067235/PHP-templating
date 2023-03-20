8541a225e43c90efa329faa5563ac2f0df05b71f
jurrian-triplence <jurrian@triplence.com>
2023-03-20 13:29:27 +0100

    Templating 3 afgemaakt.

---
+++ templating-3/index0.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include 'content.php';
+        echo '<br>';
+        echo 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis optio id magni facilis quae modi corrupti provident eligendi vero in, hic maiores quod numquam quasi voluptates cumque, excepturi nam facere!';
+    ?>
+</body>
+</html>
